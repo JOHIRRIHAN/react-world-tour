28ac19f7991f30897e7684d862f9be0861bc24fa
JOHIRRIHAN <jr7718301@gmail.com>
2024-03-17 20:43:19 +0600

    countriys

--- src/Component/Countrys/Contriys.jsx
+++ src/Component/Countrys/Contriys.jsx
@@ -1,23 +1,25 @@
 import { useEffect } from "react";
-import { useState } from "react"
+import { useState } from "react";
 import Contry from "./Contry/Contry";
-
+import "./contriys.css";
 
 const Contriys = () => {
-    const [Contriys, setCountrys] = useState([]);
-    useEffect(()=> {
-        fetch('https://restcountries.com/v3.1/all')
-        .then(res => res.json())
-        .then(data => setCountrys(data))
-    },[])
+  const [Contriys, setCountrys] = useState([]);
+  useEffect(() => {
+    fetch("https://restcountries.com/v3.1/all")
+      .then((res) => res.json())
+      .then((data) => setCountrys(data));
+  }, []);
   return (
-    <div>
-        <h3>Contriys: {Contriys.length}</h3>
-    {
-        Contriys.map(contry => <Contry key={contry.cca3} contry={contry }></Contry>)
-    }
+    <div >
+      <h3>Contriys: {Contriys.length}</h3>
+      <div className="country-container">
+        {Contriys.map((contry) => (
+          <Contry key={contry.cca3} contry={contry}></Contry>
+        ))}
+      </div>
     </div>
   );
 };
 
-export default Contriys
+export default Contriys;
--- src/Component/Countrys/Contry/Contry.jsx
+++ src/Component/Countrys/Contry/Contry.jsx
@@ -1,13 +1,26 @@
+import { useState } from 'react';
 import './Contry.css'
 const Contry = ({contry}) => {
-    const {name, flags } = contry;
-  return (
+    const {name, flags, population, area, cca3} = contry;
+  const [visited, setVisited] = useState(false)
+  const handelVisited = () => {
+    setVisited(true)
+  }
+ 
+    return (
     <div className='contry'>
         <h3>name: {name?.common}</h3>
         <h3>Republic: {name?.official}</h3>
         <img src={flags.png} alt="" />
+        <h3>population: {population}</h3>
+        <h3>area: {area}</h3>
+        <h3>cca3: {cca3}</h3>
+        <button onClick={handelVisited}>visited</button>
+        {
+            visited && 'I have visited this country.'
+        }
     </div>
   )
-}
+} 
 
 export default Contry
